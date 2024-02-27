efc78ded90c88954bfbb90966c3c61edb02c8ac0
amrelstohy <amramr2901@gmail.com>
2024-02-28 00:28:11 +0200

    hi

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_is_root - check if the node is root or not
- * 
+ *
  * @node: the node that will be checked
- * 
+ *
  * Return: 1 means the node is root and 0 vice versa
 */
 
@@ -17,4 +17,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 		return (1);
 	else
 		return (0);
+
 }
