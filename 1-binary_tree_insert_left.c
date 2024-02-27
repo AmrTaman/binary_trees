fe72719d7c96f2ab5c1404e1186fce653d123af8
amrelstohy <amramr2901@gmail.com>
2024-02-27 22:05:40 +0200

    hi

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,10 +2,10 @@
 
 /**
  * binary_tree_insert_left - isert lift
- * 
+ *
  * @parent: the parent
  * @value: the value
- * 
+ *
  * Return: node of binary_tree_t type
 */
 
@@ -13,18 +13,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
 
-	if(parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	node = malloc(sizeof(binary_tree_t));
-	if(node == NULL)
+	if (node == NULL)
 		return (NULL);
 
-	if(parent->left == NULL)
+	if (parent->left == NULL)
 	{
 		parent->left = node;
 		node->parent = parent;
-		node->n= value;
+		node->n = value;
 		node->left = NULL;
 		node->right = NULL;
 	return (node);
@@ -39,5 +39,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = node;
 		return (node);
 	}
-	return(NULL);
+	return (NULL);
 }
