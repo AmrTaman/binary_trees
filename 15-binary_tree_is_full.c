c5d917c0cf6aea260eb6cb53e22d64b85a29ffda
AmrTaman <amrtaman4325@gmail.com>
2024-02-29 10:07:34 +0200

    d

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -15,7 +15,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	left = binary_tree_is_full(tree->left);
 	right = binary_tree_is_full(tree->right);
-	if (left ^ right)
+	if (left == right)
 		return (1);
 	return (0);	
 }
