4ca7d6ddcab96fb9536a5f2a05f87a920d3cd8cf
amrelstohy <amramr2901@gmail.com>
2024-02-29 21:32:11 +0200

    hi

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -17,12 +17,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->left == node)
-		continue;
+		return (node->parent->right);
 	else
 		return (node->parent->left);
-
-	if (node->parent->right == node)
-			continue;
-		else
-			return (node->parent->right)
 }
