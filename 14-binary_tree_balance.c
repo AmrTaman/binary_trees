76f0e0ad35b14c18e2715cbcffec1637b137ff61
AmrTaman <amrtaman4325@gmail.com>
2024-02-29 09:37:25 +0200

    d

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,11 +29,11 @@ int binary_tree_heights(const binary_tree_t *tree)
  *
  * @tree: tree pointer
  *
- * Returns: balance factor
+ * Return: balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int hl, hr = 0;
+	int hl = 0, hr = 0;
 
 	if (tree == NULL)
 		return (0);
