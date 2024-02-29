68752b81eaf2605bede133619d9d03ec756f57a5
AmrTaman <amrtaman4325@gmail.com>
2024-02-29 09:31:46 +0200

    d

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_heights - gives the hight of a tree
+ *
+ * @tree: binary tree pointer
+ *
+ * Return: int number or zero if function fails
+ */
+int binary_tree_heights(const binary_tree_t *tree)
+{
+	int hlf = 0, hrg = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		hlf = 1 + binary_tree_heights(tree->left);
+	if (tree->right)
+		hrg = 1 + binary_tree_heights(tree->right);
+
+	if (hlf > hrg)
+		return (hlf);
+	return (hrg);
+}
+
+/**
+ * binary_tree_balance - calculates balance of a function
+ *
+ * @tree: tree pointer
+ *
+ * Returns: balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int hl,hr = 0;
+    if (tree == NULL)
+        return (0);
+    hl = binary_tree_heights(tree->left);
+	hr = binary_tree_heights(tree->right);
+	return (hl - hr);
+}
