22837b50276cd8378ee911b5d156064e71a7dba9
AmrTaman <amrtaman4325@gmail.com>
2024-02-29 08:08:26 +0200

    e

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,10 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth = 0;
+
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_depth(tree->parent) + 1);
+	depth = binary_tree_depth(tree->parent) + 1;
+	return (depth);
 }
